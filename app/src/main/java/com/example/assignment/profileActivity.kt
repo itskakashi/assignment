b9b8816f6e40f2312d2812aeb8a264b9d672dc3a
Satyam Mishra <153286591+itskakashi@users.noreply.github.com>
2025-05-19 01:49:57 +0530

    Update profileActivity.kt

--- app/src/main/java/com/example/assignment/profileActivity.kt
+++ app/src/main/java/com/example/assignment/profileActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight // Kept for the right arrow
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight 
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
